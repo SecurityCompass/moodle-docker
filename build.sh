23fc04ce9d2df1892af941f0ee4ea502a0aa7e35
Hrdayesh Patel <hpatel@securitycompass.com>
2018-04-17 15:15:04 -0400

    Add build script for moodle-app

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd moodle-app && docker build -t moodle-app-3.4.2 . && cd ..
+
 cd nginx && docker build -t moodle-nginx . && cd ..
 
 cd php-fpm && docker build -t moodle-fpm . && cd ..
