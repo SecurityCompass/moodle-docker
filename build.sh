2df2b0d00c64edd2ed5fa61b7f53afc45a2061f3
Hrdayesh Patel <hpatel@securitycompass.com>
2018-04-18 14:59:05 -0400

    Update PHP container name

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ cd moodle-app && docker build -t moodle-app-3.4.2 . && cd ..
 
 cd nginx && docker build -t moodle-nginx . && cd ..
 
-cd php-fpm && docker build -t moodle-fpm . && cd ..
+cd php-fpm && docker build -t moodle-php-fpm . && cd ..
