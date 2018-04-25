7ce9e22b6a47af9ef994c9bdd6a518c2af3ca6e2
Hrdayesh Patel <hpatel@securitycompass.com>
2018-04-25 09:41:52 -0400

    Don't need this file since we build from docker-compose

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd moodle-app && docker build -t moodle-app:3.4.2 . && cd ..
-
-cd nginx && docker build -t moodle-nginx . && cd ..
-
-cd php-fpm && docker build -t moodle-php-fpm . && cd ..
