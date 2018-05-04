a01a72dce20d6967aad772b185f5645b55b34543
Hrdayesh Patel <hpatel@securitycompass.com>
2018-05-04 12:11:30 -0400

    update comment

--- nginx-php/start-nginx.sh
+++ nginx-php/start-nginx.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/with-contenv sh
 set -e;
 
-#/bin/wait-for-it.sh -t 120 127.0.0.1:9000
+# Wait 5s for PHP-FPM to come up
 sleep 5
 
 envsubst '${MOODLE_DATAROOT} ${MOODLE_VERSION}' < /etc/nginx/conf.d/moodle.template > /etc/nginx/conf.d/moodle.conf && nginx -g 'daemon off;'
