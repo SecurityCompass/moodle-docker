8e50d048113c7d3763d18ef8160162d4ed7707f5
Hrdayesh Patel <hpatel@securitycompass.com>
2018-06-05 11:21:24 -0400

    Remove unused files

--- deprecated/nginx-php-ubuntu/start-nginx.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/with-contenv sh
-set -e;
-
-# Wait 5s for PHP-FPM to come up
-sleep 5
-
-envsubst '${MOODLE_DATAROOT} ${MOODLE_VERSION}' < /etc/nginx/conf.d/moodle.template > /etc/nginx/conf.d/moodle.conf && nginx -g 'daemon off;'
