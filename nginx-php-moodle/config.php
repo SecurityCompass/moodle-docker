962ce7143a12690b080d9c37831414fe331e96e2
Hrdayesh Patel <hpatel@securitycompass.com>
2018-06-05 11:24:30 -0400

    Fix typo

--- nginx-php-moodle/config.php
+++ nginx-php-moodle/config.php
@@ -14,7 +14,7 @@
 //                                                                       //
 // Copyright (C) 1999 onwards  Martin Dougiamas  http://moodle.com       //
 //                                                                       //
-// This program is free software; you ca redistribute it and/or modify  //
+// This program is free software; you can redistribute it and/or modify  //
 // it under the terms of the GNU General Public License as published by  //
 // the Free Software Foundation; either version 3 of the License, or     //
 // (at your option) any later version.                                   //
