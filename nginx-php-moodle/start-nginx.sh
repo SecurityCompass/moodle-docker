1cc76bc288fc3cda62f200561ef54b93d3f378bc
Hrdayesh Patel <hpatel@securitycompass.com>
2018-08-03 19:09:55 -0400

    Add log line Update comment and error message

--- nginx-php-moodle/start-nginx.sh
+++ nginx-php-moodle/start-nginx.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/with-contenv bash
 set -e;
 
+echo "Starting Nginx"
+
 # Source shtdlib
 . /usr/local/bin/shtdlib.sh
 
-# Wait 10s for PHP-FPM to come up
+# Wait for PHP-FPM to start Nginx
 if wait_for_success "/etc/init.d/php7.2-fpm status"; then
     envsubst '${MOODLE_DATAROOT} ${MOODLE_VERSION}' < /etc/nginx/conf.d/moodle.template > /etc/nginx/conf.d/moodle.conf && nginx -g 'daemon off;'
 else
-    echo "Error starting PHP-FPM."
+    echo "PHP-FPM did not start in time. Aborting Nginx start up."
     exit 1
 fi
