e03f801b403f9efc7309a185beed1d1ae42a42a0
Hrdayesh Patel <hpatel@securitycompass.com>
2018-08-09 09:51:58 -0400

    Add a useful message

--- nginx-php-moodle/bin/check_db.php
+++ nginx-php-moodle/bin/check_db.php
@@ -1,4 +1,5 @@
 <?php
+    echo "Checking DB connection..."
     $connection = pg_connect("host=".getenv("PGSQL_HOSTNAME")." dbname=".getenv("PGSQL_DATABASE")." user=".getenv("PGSQL_USER")." password=".getenv("PGSQL_PASSWORD"));
     if($connection) {
         exit;
