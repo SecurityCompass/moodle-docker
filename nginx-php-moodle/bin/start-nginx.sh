58d3f9fb20c24b4a3d65d596464551cfe1a924ab
Hrdayesh Patel <hpatel@securitycompass.com>
2018-08-15 14:15:20 -0400

    More shellcheck fixes

--- nginx-php-moodle/bin/start-nginx.sh
+++ nginx-php-moodle/bin/start-nginx.sh
@@ -9,6 +9,7 @@ source /usr/local/bin/shtdlib.sh
 
 # Wait for PHP-FPM to start Nginx
 if wait_for_success "/etc/init.d/php7.2-fpm status"; then
+    # shellcheck disable=SC2016
     envsubst '${MOODLE_DATAROOT} ${MOODLE_VERSION}' < /etc/nginx/conf.d/moodle.template > /etc/nginx/conf.d/moodle.conf && nginx -g 'daemon off;'
 else
     echo "PHP-FPM did not start in time. Aborting Nginx start up."
