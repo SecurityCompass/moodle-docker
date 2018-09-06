82493dd638fc2b0e37c0282256cf34148fdfb1fa
Hrdayesh Patel <hpatel@securitycompass.com>
2018-09-06 18:37:58 -0400

    Build DEB file

---
+++ bin/travis-ci/deb_build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt-get --yes install tree
+
+# Build DEB package
+docker-compose --file dc.deb.yml up
+
+# Verify DEB file content
+DEB_FILE=$(ls ./*.deb)
+ls -la "${DEB_FILE}"
+dpkg -e "${DEB_FILE}" /tmp/moodle_deb
+dpkg -x "${DEB_FILE}" /tmp/moodle_deb
+tree /tmp/moodle_deb
