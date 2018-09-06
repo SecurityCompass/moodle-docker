2d4118f512e620b598988bee5c4c0fa93e2ee4ee
Hrdayesh Patel <hpatel@securitycompass.com>
2018-09-06 18:53:15 -0400

    Oops.. forgot to build fpm container first

--- bin/travis-ci/deb_build.sh
+++ bin/travis-ci/deb_build.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-sudo apt-get --yes install tree
-
-# Build DEB package
-docker-compose --file dc.deb.yml up
-
-# Verify DEB file content
-DEB_FILE=$(ls ./*.deb)
-ls -la "${DEB_FILE}"
-dpkg -e "${DEB_FILE}" /tmp/moodle_deb
-dpkg -x "${DEB_FILE}" /tmp/moodle_deb
-tree /tmp/moodle_deb
+# Build fpm image
+docker-compose --file dc.deb.yml build --no-cache deb-build
+docker images
