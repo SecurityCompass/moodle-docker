25478b309e7199ffa92b6fc81b1ead94f65e4134
Hrdayesh Patel <hpatel@securitycompass.com>
2018-09-11 11:32:30 -0400

    Add more conditions

--- bin/travis-ci/check_tag.sh
+++ bin/travis-ci/check_tag.sh
@@ -28,7 +28,10 @@ echo "${changelog_ver}" | grep -qP ${version_pattern} || ( echo "Invalid tag fro
 echo "${iteration_ver}" | grep -qP ${version_pattern} || ( echo "Invalid iteration from DEB configuration: '${iteration_ver}'" && exit 1 )
 
 # Ensure that we tag relevant files in each PR
-if ! compare_versions "${latest_tag}" "${changelog_ver}" || ! compare_versions "${latest_tag}" "${iteration_ver}"; then
+if "${latest_tag}" = "${changelog_ver}" \
+   || "${latest_tag}" = "${iteration_ver}" \
+   || ! compare_versions "${latest_tag}" "${changelog_ver}" \
+   || ! compare_versions "${latest_tag}" "${iteration_ver}"; then
     echo "Error: Version in CHANGELOG.md and 'dc.deb.yml' not updated"
     exit 1
 fi
