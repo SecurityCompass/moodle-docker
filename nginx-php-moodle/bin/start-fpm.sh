4f9b0475d9ab43c4068a4cefa9c20fc19a172716
Hrdayesh Patel <hpatel@securitycompass.com>
2018-09-11 14:52:49 -0400

    Bash style update

--- nginx-php-moodle/bin/start-fpm.sh
+++ nginx-php-moodle/bin/start-fpm.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/with-contenv sh
-set -e;
+#
+# Copyright (c) 2018 SD Elements Inc.
+#
+#  All Rights Reserved.
+#
+# NOTICE:  All information contained herein is, and remains
+# the property of SD Elements Incorporated and its suppliers,
+# if any.  The intellectual and technical concepts contained
+# herein are proprietary to SD Elements Incorporated
+# and its suppliers and may be covered by U.S., Canadian and other Patents,
+# patents in process, and are protected by trade secret or copyright law.
+# Dissemination of this information or reproduction of this material
+# is strictly forbidden unless prior written permission is obtained
+# from SD Elements Inc..
+# Version
+
+set -eo pipefail
 
 echo "Starting PHP-FPM"
 
