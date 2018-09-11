b483a8800bbefafaeefff695db53d98af3c0aed7
Hrdayesh Patel <hpatel@securitycompass.com>
2018-09-11 14:43:48 -0400

    Update variable style

--- bin/travis-ci/docker_publish.sh
+++ bin/travis-ci/docker_publish.sh
@@ -3,7 +3,7 @@ set -e
 set -o pipefail
 
 # Log into our Docker registry
-echo "$DOCKER_REGISTRY_PASSWORD" | docker login -u "$DOCKER_REGISTRY_USER" --password-stdin "${DOCKER_REGISTRY_URL}"
+echo "${DOCKER_REGISTRY_PASSWORD}" | docker login -u "${DOCKER_REGISTRY_USER}" --password-stdin "${DOCKER_REGISTRY_URL}"
 
 # Push image
 docker-compose --file docker-compose.yml --file dc.build.yml push nginx-php-moodle
